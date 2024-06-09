ba1213a0ee4d5f86ddbd11980e403024a3519881
evanrich2404 <evanrich2404@gmail.com>
2024-06-09 16:03:51 -0500

    This should work :0

---
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -0,0 +1,10 @@
+main(args) {
+  int number = int.parse(args[0]);
+  if (number > 0) {
+    print('$number is positive');
+  } else if (number == 0) {
+    print('$number is zero');
+  } else {
+    print('$number is negative');
+  };
+}
