91c8fa1e4b6f0f97928642d5de58b1fa0969b26c
evanrich2404 <evanrich2404@gmail.com>
2024-06-26 15:51:11 -0500

    get sum

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,24 @@
+import '4-util.dart';
+import 'dart:convert';
+
+Future<double> calculateTotal() async {
+  try {
+    String userData = await fetchUserData();
+    var decodedUserData = jsonDecode(userData);
+    String userId = decodedUserData['id'];
+
+    String userOrders = await fetchUserOrders(userId);
+    var decodedOrders = jsonDecode(userOrders) as List;
+
+    double total = 0.0;
+    for (var product in decodedOrders) {
+      String productPriceStr = await fetchProductPrice(product);
+      double productPrice = double.parse(jsonDecode(productPriceStr).toString());
+      total += productPrice;
+    }
+    return total;
+  } catch (e) {
+    print('error caught: $e');
+    return -1;
+  }
+}
